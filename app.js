60b88b70bca6e785b11c57a2486ac2ff4d96cdac
nishantsahu <nishant.sahu106@gmail.com>
2026-01-22 21:53:35 +0530

    updated message

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "application/json" });
   res.end(
     JSON.stringify({
-      message: "Hello from basic Node server",
+      message: "Kaa sir!!!",
       time: new Date().toISOString(),
     }),
   );
